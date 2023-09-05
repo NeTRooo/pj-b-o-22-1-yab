5f98796fafd4a959128c2c5a9bec78c7eef86402
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-09-05 11:58:02 +0300

    Проверка всех пользователей

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ def check_ecampus(url):
     while True:
         response = requests.get(url)
         data = response.json()
-        if data[0]['uuid'] is not None:
-            print('Пользователь существует')
-        else:
-            print('Пользователь не существует')
+        for i in range(0, 10):
+            if data[i] is not None:
+                print('Пользователь существует')
+            else:
+                print('Пользователь не существует')
         time.sleep(600)
 
 if __name__ == '__main__':
