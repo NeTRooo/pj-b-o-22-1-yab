a2d7d2c9419de4e307af86485808d0bede98de3a
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-09-05 12:00:12 +0300

    Добавлен обработчик ошибок

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import requests
 import time
 import json
 
-def check_ecampus(url):
+def check_users(url):
     while True:
         response = requests.get(url)
         data = response.json()
         for i in range(0, 10):
-            if data[i] is not None:
-                print('Пользователь существует')
-            else:
+            try:
+                if data[i] is not None:
+                    print('Пользователь существует')
+                else:
+                    print('Пользователь не существует')
+            except Exception as e:
                 print('Пользователь не существует')
         time.sleep(600)
 
 if __name__ == '__main__':
-    check_ecampus(url='https://api.heylon.ru/api/players')
+    check_users(url='https://api.heylon.ru/api/players')
