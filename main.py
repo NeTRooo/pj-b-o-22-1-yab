5906d95aea3559c4c0b08d2851b3ba93c47d47f3
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-09-05 12:04:04 +0300

    Мини обновление

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def check_users(url):
         for i in range(0, 10):
             try:
                 if data[i] is not None:
-                    print('Пользователь существует')
+                    print(f"Пользователь {data[i]['nick']} - существует")
                 else:
-                    print('Пользователь не существует')
+                    print("Пользователь не существует")
             except Exception as e:
-                print('Пользователь не существует')
-        time.sleep(600)
+                print("Пользователь не существует")
+        time.sleep(300)
 
 if __name__ == '__main__':
     check_users(url='https://api.heylon.ru/api/players')
